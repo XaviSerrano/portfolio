a2359401f4a056fe78f03e995424aca4d3161afe
Ivax7 <xserrano2001@gmail.com>
2026-01-26 18:23:55 +0100

    Habilidades section

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Nav } from '../components/nav/nav';
 import { Introduction } from '../components/introduction/introduction';
 import { Projects } from '../components/projects/projects';
+import { Skills } from '../components/skills/skills';
 
 @Component({
   selector: 'app-home',
-  imports: [Nav, Introduction, Projects],
+  imports: [Nav, Introduction, Projects, Skills],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
