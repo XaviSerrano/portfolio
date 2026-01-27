f8c7f24d7d5a960a15ae260ce6a15ddd0ab3be78
Ivax7 <xserrano2001@gmail.com>
2026-01-27 21:14:49 +0100

    Estudios, About & Footer

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,10 +3,13 @@ import { Nav } from '../components/nav/nav';
 import { Introduction } from '../components/introduction/introduction';
 import { Projects } from '../components/projects/projects';
 import { Skills } from '../components/skills/skills';
+import { Studies } from '../components/studies/studies';
+import { About } from '../components/about/about';
+import { Footer } from '../components/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Nav, Introduction, Projects, Skills],
+  imports: [Nav, Introduction, Projects, Skills, Studies, About, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
