a49efcf29d469d813c4753f49e66d1e1d8974ffc
Ivax7 <xserrano2001@gmail.com>
2026-01-29 19:04:35 +0100

    Responsive

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule, FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faCopy, faCheck } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
 
+  faCopy = faCopy;
+  faCheck = faCheck;
+  copied = false;
+  email = "xserrano2001@gmail.com";
+
+  copyEmail() {
+    navigator.clipboard.writeText(this.email);
+    this.copied = true;
+    console.log(this.copied)
+    setTimeout(() => {
+    this.copied = false,
+    console.log(this.copied)
+  }
+    , 2000);
+  }
+
+  openCV() {
+    window.open('/assets/docs/XavierSerranoCV.pdf', '_blank');
+  }
 }
